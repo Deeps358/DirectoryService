2a49366658b74ac6ced349de92ecc507679fa7ac
Deeps <toskin.dv@gmail.com>
2025-10-31 18:57:31 +0300

    Переделал с сервисов на хэндлеры

---
+++ DirectoryService/src/DirectoryServices.Application/Abstractions/ICommandHandler.cs
@@ -0,0 +1,19 @@
+﻿using CSharpFunctionalExtensions;
+using Shared.ResultPattern;
+
+namespace DirectoryServices.Application.Abstractions
+{
+    public interface ICommand;
+
+    public interface ICommandHandler<TResponse, in TCommand>
+        where TCommand : ICommand
+    {
+        Task<Shared.ResultPattern.Result<TResponse>> Handle(TCommand command, CancellationToken cancellationToken);
+    }
+
+    public interface ICommandHandler<in TCommand>
+        where TCommand : ICommand
+    {
+        Task<UnitResult<Error>> Handle(TCommand command, CancellationToken cancellationToken);
+    }
+}
