6bbda6a67eb661be7bf25083d332bcfcba3eca87
Deeps <toskin.dv@gmail.com>
2026-01-25 18:18:25 +0300

    кэш для топ-5 депов по позициям

--- DirectoryService/src/DirectoryServices.Application/Departaments/Queries/GetTopFiveByPositions/GetTopFiveByPositionsHandler.cs
+++ DirectoryService/src/DirectoryServices.Application/Departaments/Queries/GetTopFiveByPositions/GetTopFiveByPositionsHandler.cs
@@ -1,7 +1,9 @@
-using DirectoryServices.Application.Abstractions;
+﻿using DirectoryServices.Application.Abstractions;
 using DirectoryServices.Application.Database;
+using DirectoryServices.Application.Departaments.Queries.GetRoots;
 using DirectoryServices.Contracts.Departaments;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Caching.Hybrid;
 using Microsoft.Extensions.Logging;
 
 namespace DirectoryServices.Application.Departaments.Queries.GetTopFiveByPositions
@@ -9,27 +11,51 @@ namespace DirectoryServices.Application.Departaments.Queries.GetTopFiveByPositio
     public class GetTopFiveByPositionsHandler : IQueryHandler<GetTopFiveByPositionsDto[], GetTopFiveByPositionsQuery>
     {
         private readonly IReadDbContext _readDbContext;
+        private readonly HybridCache _hybridCache;
         private readonly ILogger<GetTopFiveByPositionsHandler> _logger;
 
-        public GetTopFiveByPositionsHandler(IReadDbContext readDbContext, ILogger<GetTopFiveByPositionsHandler> logger)
+        public GetTopFiveByPositionsHandler(
+            IReadDbContext readDbContext,
+            HybridCache hybridCache,
+            ILogger<GetTopFiveByPositionsHandler> logger)
         {
             _readDbContext = readDbContext;
+            _hybridCache = hybridCache;
             _logger = logger;
         }
 
         public async Task<GetTopFiveByPositionsDto[]?> Handle(GetTopFiveByPositionsQuery query, CancellationToken cancellationToken)
         {
-            return await _readDbContext.DepartamentsRead
-                .Select(d => new GetTopFiveByPositionsDto
+            return await GetTopFiveByPositions(query, cancellationToken);
+        }
+
+        private async Task<GetTopFiveByPositionsDto[]> GetTopFiveByPositions(
+            GetTopFiveByPositionsQuery query,
+            CancellationToken cancellationToken)
+        {
+            string topFiveDepsKey = "topfivedeps";
+
+            GetTopFiveByPositionsDto[] topFiveDepsCache = await _hybridCache.GetOrCreateAsync<GetTopFiveByPositionsDto[]>(
+                key: topFiveDepsKey,
+                factory: async _ =>
                 {
-                    Id = d.Id.Value,
-                    Name = d.Name.Value,
-                    PositionsCount = d.Positions.Count,
-                    IsActive = d.IsActive,
-                })
-                .OrderByDescending(d => d.PositionsCount)
-                .Take(5)
-                .ToArrayAsync();
+                    GetTopFiveByPositionsDto[] topFiveDepsResponce = await _readDbContext.DepartamentsRead
+                        .Select(d => new GetTopFiveByPositionsDto
+                        {
+                            Id = d.Id.Value,
+                            Name = d.Name.Value,
+                            PositionsCount = d.Positions.Count,
+                            IsActive = d.IsActive,
+                        })
+                        .OrderByDescending(d => d.PositionsCount)
+                        .Take(5)
+                        .ToArrayAsync(cancellationToken);
+
+                    return topFiveDepsResponce;
+                },
+                cancellationToken: cancellationToken);
+
+            return topFiveDepsCache;
         }
     }
 }
